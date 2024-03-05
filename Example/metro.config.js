2fb308c4bf05bb881b271c7ff9dd18e131d5621d
1010nishant <nishantjangid6377@gmail.com>
2024-03-06 02:52:30 +0530

    initial commit

---
+++ Example/metro.config.js
@@ -0,0 +1,38 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const moduleRoot = path.resolve(__dirname, '..');
+
+module.exports = {
+  watchFolders: [moduleRoot],
+  resolver: {
+    extraNodeModules: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+      'react-native-paper': path.resolve(
+        __dirname,
+        'node_modules/react-native-paper',
+      ),
+    },
+    blockList: exclusionList([
+      new RegExp(`${moduleRoot}/node_modules/react/.*`),
+      new RegExp(`${moduleRoot}/node_modules/react-native/.*`),
+      new RegExp(`${moduleRoot}/node_modules/react-native-paper/.*`),
+    ]),
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
